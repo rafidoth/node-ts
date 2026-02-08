3b6534480d2395a96bbf2da639c314205fd1eea0
rafidoth <rafiulhasan803@gmail.com>
2026-02-08 10:11:01 +0600

    remove unnecessary message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ import { authRouter } from "@/routes/auth.route";
 export const registerRoutes = (app: Router) => {
     const v1_router = Router()
     v1_router.get("/health", (_req, res) => {
-        res.status(200).json({ status: "ok", service: "auth-service" });
+        res.status(200).json({ status: "ok" });
     });
     v1_router.use("/auth", authRouter)
 
